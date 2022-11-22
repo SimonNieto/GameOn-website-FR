f9302f736120a0f2c9f277bf302a17c20d10c7b6
sim <eslsimon59@gmail.com>
2022-11-22 12:35:44 +0100

    Fermeture de la modal sur x

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
+const modalCloseBtn = document.querySelectorAll(".close");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -20,4 +20,11 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// Close modal event
+modalCloseBtn[0].addEventListener ("click", closeModal);
 
